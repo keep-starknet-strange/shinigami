115e78acb65178eea5b9e5531bfe4106687dd39c
supreme2580 <victoromorogbe69@gmail.com>
2024-08-19 21:03:41 +0100

    wip

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -30,10 +30,11 @@ export default function Home() {
               </div>
             </div>
             <div className="flex flex-row items-center justify-center w-full">
-              <div className="w-full max-w-4xl flex flex-row items-center justify-between">
+              <div className="w-full max-w-4xl flex flex-col items-center justify-between space-y-5">
+                {/* Script editor */}
                 <div className="w-full">
                   <div className="w-full flex flex-row items-center justify-between">
-                    <div className="w-36 h-10 bg-[#0E0E0E] clip-trapezium flex flex-col items-start justify-center pl-2.5 pt-1.5 rounded-t-xl">
+                    <div className="w-36 h-10 bg-[#0E0E0E] clip-trapezium-right flex flex-col items-start justify-center pl-2.5 pt-1.5 rounded-t-xl">
                       <p className="text-[#85FFB2]">Script Editor</p>
                     </div>
                     <button className="flex flex-row items-center space-x-1">
@@ -54,6 +55,39 @@ export default function Home() {
                       <p className="text-white">REFRESH</p>
                     </button>
                   </div>
+                </div>
+                {/* Stack Visualizer */}
+                <div className="w-full bg-[#0E0E0E] h-fit rounded-lg rounded-b-xl pt-0.5">
+                  <div className="w-full flex flex-row items-center justify-between pr-1.5">
+                    <div className="w-44 h-12 bg-[#0E0E0E] clip-trapezium-right flex flex-col items-start justify-center pl-2.5 pt-1.5 rounded-t-xl">
+                      <p className="text-[#85FFB2]">Stack Visualizer</p>
+                    </div>
+                    <div className="w-48 h-12 bg-[#0E0E0E] clip-trapezium-left flex flex-col items-start justify-center px-1.5 pt-1.5 rounded-t-xl">
+                      <button className="w-full h-full text-right pr-2.5 bg-[#00FF5E] clip-trapezium-inner-left rounded-r-sm rounded-bl-sm text-black">GENERATE PROOF</button>
+                    </div>
+                  </div>
+                  <div className="w-full border-8 border-[#0E0E0E] h-fit">
+                    <div className="py-2.5 rounded-t-lg flex flex-row space-x-5 px-3.5 w-full bg-black">
+                      <span>ID</span>
+                      <span>Value</span>
+                    </div>
+                    <div className="w-full h-[1px] bg-[#2B2B2B]" />
+                    {/* render data in this div */}
+                    <div className="w-full bg-black h-40 rounded-b-lg" />
+                    <div className="w-full bg-black h-48 rounded-t-lg rounded-b-lg mt-2.5">
+                      <div className="py-2.5 rounded-t-lg rounded-b-lg flex flex-row space-x-5 px-3.5 w-full bg-black">
+                        <span>Proof of Status:</span>
+                      </div>
+                      <div className="w-full h-[1px] bg-[#2B2B2B]" />
+                      <div className="w-full px-2.5 pt-1 h-40">
+                        <p className="text-[#959595] text-sm">Ready to generate...</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Footer */}
+                <div className="flex flex-row items-center justify-center pt-5 pb-8">
+                  <p className="text-white text-lg">SHINIGAMI SCRIPT WIZARD. V.10</p>
                 </div>
               </div>
             </div>
