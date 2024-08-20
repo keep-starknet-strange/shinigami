17b77f303f8ca8a1c1c003ded05904f7f2072ab9
supreme2580 <victoromorogbe69@gmail.com>
2024-08-20 16:17:16 +0100

    about modal added

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,12 +4,19 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/logo.png";
 import githubImage from "@/images/github.svg";
+import telegram from "@/images/telegram.svg";
 import menu from "@/images/menu.svg";
 import x_circle from "@/images/x-circle.svg";
+import cancel_circle from "@/images/cancel-circle.svg";
 import { useState } from "react";
+import {
+    Dialog,
+} from "@material-tailwind/react";
 
 export default function Header() {
     const [open, setOpen] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+    const handleOpen = () => setOpenModal(!open);
     return (
         <div className="w-full h-24 border-y border-white/10 flex flex-col justify-center sm:h-fit sm:border-y-0 sm:flex-row">
             <div className="w-full max-w-4xl flex flex-row items-center justify-between border border-white/10 px-3.5 rounded-3xl sm:rounded-none sm:border-0">
@@ -23,7 +30,7 @@ export default function Header() {
                     <Image src={menu} alt="menu" />
                 </button>
                 <div className="sm:flex flex-row items-center space-x-5 hidden">
-                    <h6 className="uppercase text-white">About</h6>
+                    <button className="uppercase text-white" onClick={handleOpen}>About</button>
                     <Link
                         href="https://github.com/keep-starknet-strange/shinigami"
                         target="_blank"
@@ -35,6 +42,33 @@ export default function Header() {
                     </Link>
                 </div>
             </div>
+            <Dialog open={openModal} handler={handleOpen} className="bg-black w-full">
+                <button className="p-0.5 border-2 border-[#00FF5E] rounded-md absolute -top-3.5 -right-3 bg-[#00FF5E]/10 z-20" onClick={() => setOpenModal(false)}>
+                    <Image src={cancel_circle} alt="close" className="z-20" />
+                    <div className="w-2.5 h-2.5 bg-black absolute top-2.5 left-2.5 -z-10" />
+                </button>
+                <div className="w-full h-full pb-3.5">
+                    <div className="w-full h-24 bg-[url('/banner.png')] bg-cover bg-no-repeat bg-center" />
+                    <div className="p-2.5 font-medium">
+                        <p className="text-white">shinigami is a library enabling Bitcoin Script VM execution in Cairo, thus allowing the generation of STARK proofs of generic Bitcoin Script computation.</p>
+                        <ul className="list-disc text-white ml-3.5">
+                            <li>Bitcoin script interpretation and execution</li>
+                            <li>Easily configurable VM (enable different opcodes)</li>
+                            <li>In cairo, Bitcoin Script compiler</li>
+                        </ul>
+                    </div>
+                    <div className="flex flex-row items-center space-x-2.5 px-2.5">
+                        <button className="flex flex-row items-center space-x-1 bg-[#00FF5E]/10 border-[#00FF5E] border-2 py-2.5 px-5">
+                            <Image src={githubImage} alt="" unoptimized />
+                            <h6 className="text-[#00FF5E] uppercase text-base">GITHUB</h6>
+                        </button>
+                        <button className="flex flex-row items-center space-x-1 bg-[#00FF5E]/10 border-[#00FF5E] border-2 py-2.5 px-5">
+                            <Image src={telegram} alt="" unoptimized />
+                            <h6 className="text-[#00FF5E] uppercase text-base">TELEGRAM</h6>
+                        </button>
+                    </div>
+                </div>
+            </Dialog>
             {open && <div className="w-full h-fit absolute top-16 z-20 inset-x-0 flex flex-row items-center rounded-xl sm:hidden">
                 <div className="w-full mx-5 h-full backdrop-blur-md rounded-xl border-2 border-white/10">
                     <div className="w-full max-w-4xl flex flex-row items-center justify-between border-b border-white/10 px-3.5 sm:border-0">
@@ -49,7 +83,7 @@ export default function Header() {
                         </button>
                     </div>
                     <div className="py-2.5 px-2.5 space-y-2.5">
-                        <button className="text-white w-full text-center bg-[#111111] py-2.5 rounded-md">ABOUT</button>
+                        <button className="text-white w-full text-center bg-[#111111] py-2.5 rounded-md" onClick={() => setOpenModal(true)}>ABOUT</button>
                         <button className="text-white w-full text-center bg-[#111111] py-2.5 rounded-md flex row items-center justify-center">
                             <div className="flex flex-row items-center space-x-1">
                                 <Image src={githubImage} alt="" unoptimized />
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+const config: Config = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,5 +17,5 @@ const config: Config = {
     },
   },
   plugins: [],
-};
+});
 export default config;
