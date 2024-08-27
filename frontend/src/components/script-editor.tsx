c5837d02cabaab3c5b574cf4af7ebb228a3fb0f6
supreme2580 <victoromorogbe69@gmail.com>
2024-08-27 16:09:53 +0100

    fixed height issue

--- frontend/src/components/script-editor.tsx
+++ frontend/src/components/script-editor.tsx
@@ -15,7 +15,9 @@ import { StackItem } from "../../types";
 
 // const fetcher = (url: string) => fetch(url).then((res) => res.json());
 // const fetcher = (url: string) => fetch(url).then((_) => JSON.parse(data.message));
-let data = { "message": "[\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\"]" }
+let data = {
+  "message": "[\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\",\"0x39c02658ed1416713cf4098382e80d07786eed7004fc3fd89b38c7165fdabc80\"]"
+}
 const fetcher = (url: string) => fetch(url).then((_) => JSON.parse(data.message));
 
 export default function ScriptEditor() {
