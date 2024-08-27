ec8d15612f3eb6f44176df4aba61aca8f12f12a8
supreme2580 <victoromorogbe69@gmail.com>
2024-08-27 09:11:21 +0100

    start script

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { exec } = require('child_process');
 const app = express();
 
-// Function to run shell commands
 function runShellCommand(command, callback) {
     exec(command, (error, stdout, stderr) => {
         if (error) {
@@ -22,15 +21,11 @@ app.get('/run-script', (req, res) => {
     if (!args) {
         return res.status(400).send('Missing "args" parameter');
     }
-
     const scriptPath = '../tests/text_to_byte_array.sh';
     const initialCommand = `bash ${scriptPath} ${args}`;
-
     runShellCommand(initialCommand, (firstOutput) => {
         const modifiedOutput = `[[],0,0,${firstOutput.trim().slice(1)}`;
         const cairoCommand = `scarb cairo-run ${modifiedOutput}`;
-        console.log("initial output: ", firstOutput)
-        console.log("test command: ", cairoCommand)
         runShellCommand(cairoCommand, (finalOutput) => {
             console.log("final output: ", finalOutput)
             res.send(finalOutput);
@@ -38,7 +33,6 @@ app.get('/run-script', (req, res) => {
     });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
