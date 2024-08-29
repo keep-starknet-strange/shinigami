62a23c3f2d59981c69009e814d40fc4165d0556b
Brandon Roberts <brandonjroberts22@gmail.com>
2024-08-29 00:04:49 -0400

    Infra initailized with docker and kube

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,7 @@ app.post('/run-script', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
